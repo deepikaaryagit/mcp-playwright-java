8063312584fecf0453d9ded8e580b4e52a685982
deepikaaryagit <deepika.arya@gmail.com>
2025-11-23 19:35:59 -0800

    update login page without mcp

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,7 +1,6 @@
 package pages;
 
 import com.microsoft.playwright.Page;
-import mcp.AIUtils;
 
 public class LoginPage {
 
@@ -16,12 +15,20 @@ public class LoginPage {
     }
 
     public void login(String email, String password) {
-        // For demo: get locator from MCP using snippet (pretend snippet)
-        page.locator(".sb-button.sb-button--default.sb-button--black.mr3").first().click();
-        String emailLocator = AIUtils.getSmartLocator("<input id='email' />");
 
-        page.locator(emailLocator).fill(email);
-        page.locator("#password").fill(password);
+        // Click Sign In button on homepage
+        page.locator(".sb-button.sb-button--default.sb-button--black.mr3").first().click();
+
+        // Wait until modal OR login page is loaded
+        page.waitForSelector("input[name='username'], input[type='email']");
+
+        // Enter email
+        page.locator("input[name='username'], input[type='email']").first().fill(email);
+
+        // Enter password
+        page.locator("input[name='password']").fill(password);
+
+        // Click login
         page.locator("button[type='submit']").click();
     }
 }
